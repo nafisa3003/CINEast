8fe9f9d6a7d276d0968074f46af55940e7c8cd6d
Syeda Nafisa Tasnim <nafisa.tasnim3003@gmail.com>
2026-08-03 01:11:00 +0600

    fix(structure): align application package declarations

--- src/main/java/com/cineast/config/SecurityConfig.java
+++ src/main/java/com/cineast/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.cineast.config;
 
-import com.cineast.service.UserService;
+//import com.cineast.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
--- src/main/java/com/cineast/service/CineastApplication.java
+++ src/main/java/com/cineast/service/CineastApplication.java
@@ -1,4 +1,4 @@
-package com.cineast;
+package com.cineast.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
